beae65013e08bfd7ad63e6ce5a644dd71b2f2245
Kazbek Abraliev <kazbek.abraliev@gmail.com>
2024-11-04 19:43:33 -0500

    last commit

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,10 @@
+// implement function that creates new file fresh.txt with content I am fresh and young
+// inside of the files folder (if file already exists Error with message FS operation failed must be thrown)
+
 const create = async () => {
-    // Write your code here 
+  // Write your code here
+  const filePath = path.join(__dirname, "files", "fresh.txt");
+  const fileContent = "I am fresh and young";
 };
 
 await create();
